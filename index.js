c6c00c166fd72e02c00c16db30d513a6f96695bd
Steven Halase <steven.halase@gmail.com>
2016-07-25 15:23:29 -0600

    Fixed port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 ///// Parsing json
 app.use(bodyParser.json());
